6892cfa1f2c85d7ce80b1983ee268d1a5d7b155a
Christian <christian@serverless.industries>
2020-06-21 16:49:35 +0200

    improve caching mechanism for service worker, cleanup css

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,10 +3,22 @@
 # only Main files contain this front matter, not partials.
 ---
 
+console.log('Welcome to a service worker powered website');
+
 const cacheKey = 'site-cache-v1';
 const basePath = '{{site.url}}{{"/" | relative_url}}';
 
+// list of ressources to cache
 const forceCacheList = [
+    // pages
+    '', // index.html
+    'index.html',
+    'index.en.html',
+    'offline.html',
+    'authors.html',
+    'tags.html',
+    'site.webmanifest',
+
     // assets
     'css/Datacenter_Empty_Floor-snip.jpg',
     'css/cc-80x15.png',
@@ -32,27 +44,30 @@ const forceCacheList = [
 ]
 .map(url => basePath + url);
 
-const cacheList = [
-    ... forceCacheList,
+const cacheList = forceCacheList;
 
-    ... [
-        // pages
-        '', // index.html
-        'index.html',
-        'index.en.html',
-        'offline.html',
-        'authors.html',
-        'tags.html',
-        'site.webmanifest',
-    ]
-    .map(url => basePath + url)
-];
 
 // listen for requests for page assets and serve from cache
 const failedRequestHandler = err =>
 {
     return caches.match(basePath + 'offline.html');
 }
+
+const cacheOne = async (url, response, event) =>
+{
+    var cache = await caches.open(cacheKey);
+    await cache.put(url, response.clone());
+
+    const client = await clients.get(event.source.id);
+    if (client)
+    {
+        await client.postMessage({
+            type: 'URL_CACHED',
+            uid: url,
+            result: true
+        });
+    }
+};
 
 self.addEventListener('fetch', event =>
 {
@@ -61,12 +76,22 @@ self.addEventListener('fetch', event =>
     {
         const cacheHandler = caches.match(event.request.url).then(cacheItem =>
         {
+            // cache first, request if cache not available
+            // for files in the force cache list
             if (forceCacheList.includes(event.request.url))
             {
                 // cache first
                 return cacheItem || fetch(event.request.url)
+                    .then(response =>
+                    {
+                        // add fetched ressource to cache
+                        return cacheOne(event.request.url, response, event)
+                            .then(() => response);
+                    })
                     .catch(failedRequestHandler);
             }
+            // request first, cache if request fails
+            // for files in the extended cacheList
             else
             {
                 // request first
@@ -76,12 +101,9 @@ self.addEventListener('fetch', event =>
                         // update cache if in extended cache list
                         if (cacheList.includes(event.request.url))
                         {
-                            return caches.open(cacheKey)
-                                .then(cache =>
-                                {
-                                    cache.put(event.request.url, response.clone());
-                                    return response;
-                                });
+                            // add fetched ressource to cache
+                            return cacheOne(event.request.url, response, event)
+                                .then(() => response);
                         }
 
                         return response;
@@ -94,6 +116,7 @@ self.addEventListener('fetch', event =>
     }
 });
 
+
 // check updates mechanism
 const checkUpdates = async () =>
 {
@@ -102,9 +125,7 @@ const checkUpdates = async () =>
         // get remote status
         const ts = Date.now();
         const checkUrl = basePath + 'lastUpdate.txt';
-        const response = await fetch(basePath + 'lastUpdate.txt?' + ts)
-        const cacheReponse = response.clone();
-
+        const response = await fetch(basePath + 'lastUpdate.txt?' + ts);
         const remoteTimestamp = parseInt(await response.text());
 
         // get local state
@@ -117,7 +138,21 @@ const checkUpdates = async () =>
             localTimestamp = parseInt(await cacheItem.clone().text());
         }
 
-        return localTimestamp < remoteTimestamp;
+        if (localTimestamp < 0)
+        {
+            // no local cache available
+            return 1;
+        }
+        else if (localTimestamp < remoteTimestamp)
+        {
+            // local cache expired
+            return 2;
+        }
+        else
+        {
+            // no updates
+            return 0;
+        }
     }
     catch
     {
@@ -125,50 +160,57 @@ const checkUpdates = async () =>
     }
 };
 
+
 // perform a cache update
-const performUpdate = async () =>
+const performUpdate = async (event) =>
 {
     const ts = Date.now();
-    const cache = await caches.open(cacheKey);
     const checkUrl = basePath + 'lastUpdate.txt';
 
-    // update assets
-    cacheList.forEach(url =>
-    {
-        fetch(url + '?' + ts).then(urlResponse => cache.put(url, urlResponse));
-    });
-
     // update state file
-    fetch(checkUrl + '?' + ts).then(urlResponse => cache.put(checkUrl, urlResponse));
+    const checkResponse = await fetch(checkUrl + '?' + ts);
+    await cacheOne(checkUrl, checkResponse, event);
+
+    // update assets
+    await Promise.all(cacheList.map(async url =>
+    {
+        const cacheResponse = await fetch(url + '?' + ts)
+        await cacheOne(url, cacheResponse, event);
+    }));
 };
+
 
 // listen for messages from frontend
 const messageRespond = async (event, result) =>
 {
     const client = await clients.get(event.source.id);
 
-    client.postMessage({
-        type: event.data.type + '_RESULT',
-        uid: event.data.uid,
-        result: result
-    });
+    if (client)
+    {
+        client.postMessage({
+            type: event.data.type + '_RESULT',
+            uid: event.data.uid,
+            result: result
+        });
+    }
 };
+
 
 // listen for event from frontend
 self.addEventListener('message', async event =>
 {
+    // check if a ressource is cached
     if (event.data.type === 'CHECK_OFFLINE_STATE' && event.data.uid)
     {
         var cacheItem = await caches.match(event.data.uid);
         await messageRespond(event, !!cacheItem);
     }
+    // request to add a url to cache
     else if(event.data.type === 'ADD_CACHE_URL' && event.data.uid)
     {
-        const cache = await caches.open(cacheKey);
-
         try
         {
-            await cache.put(event.data.uid, await fetch(event.data.uid));
+            await cacheOne(event.data.uid, await fetch(event.data.uid), event);
             await messageRespond(event, true);
         }
         catch
@@ -176,6 +218,7 @@ self.addEventListener('message', async event =>
             await messageRespond(event, false);
         }
     }
+    // request to remove url from cache
     else if(event.data.type === 'REMOVE_CACHE_URL' && event.data.uid)
     {
         const cache = await caches.open(cacheKey);
@@ -189,14 +232,16 @@ self.addEventListener('message', async event =>
             await messageRespond(event, false);
         }
     }
+    // check if updates available on server
     else if(event.data.type === 'CHECK_UPDATES')
     {
         const result = await checkUpdates();
         await messageRespond(event, result);
     }
+    // perform cache refresh
     else if(event.data.type === 'UPDATE_CACHE')
     {
-        await performUpdate();
+        await performUpdate(event);
         await messageRespond(event, true);
     }
 });
