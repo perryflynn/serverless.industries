205d4edf97455bf2d308120b0e3b1807d59811f8
Christian <christian@serverless.industries>
2023-10-28 22:10:49 +0200

    fix tls handling on devserver

--- start-server.sh
+++ start-server.sh
@@ -8,14 +8,14 @@ if [ ! -f src/serve-cert.pem ] || [ ! -f src/serve-key.pem ]; then
     openssl req -new -x509 -days 365 -nodes -out src/serve-cert.pem -keyout src/serve-key.pem
 fi
 
-host=""
-if [ ! -z "$JEKYLL_HOSTNAME" ]; then
-    host="--host $JEKYLL_HOSTNAME"
+args=()
+if [ -n "$JEKYLL_HOSTNAME" ]; then
+    host+=( "--host $JEKYLL_HOSTNAME" --ssl-cert serve-cert.pem --ssl-key serve-key.pem )
 fi
 
 mkdir -p vendor/ruby
 bundle config set --local path vendor/ruby
 bundle install
-bundle exec jekyll serve $host --lsi --unpublished --ssl-cert serve-cert.pem --ssl-key serve-key.pem -s ./src -d ./_site --config ./src/_config.yml,./src/_config_staging.yml,./src/_config_ci_demo.yml
+bundle exec jekyll serve --lsi --unpublished "${args[@]}" -s ./src -d ./_site --config ./src/_config.yml,./src/_config_staging.yml,./src/_config_ci_demo.yml
 
 exit 0
