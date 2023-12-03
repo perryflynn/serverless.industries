c14da56e017467f7bab280cc58c3f7a28f48dea6
Christian <christian@serverless.industries>
2023-12-03 20:39:53 +0100

    codeformatting frontpage plugin

--- src/_plugins/frontpage.rb
+++ src/_plugins/frontpage.rb
@@ -1,21 +1,24 @@
 module FrontPagePlugin
-
     class FrontPageGenerator < Jekyll::Generator
+
         safe true
 
         def generate(site)
 
+            # group by ref or id
             byref = site.posts.docs.group_by { |p| p.data['ref'] || p.id }
 
             byref.each do |key, posts|
+                # sort locale in reverse order, so that en wins against de
                 posts.sort_by { |post| post.data['locale'] }.reverse.each_with_index do |post, postindex|
                     if postindex <= 0
+                        # add frontpage category to first post in one ref group
                         post.data['categories'].push('frontpage')
                     end
                 end
             end
 
         end
-    end
 
+    end
 end
