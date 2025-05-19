61113d3abf1ff25d5ac79df8017f05b93d5e5492
Christian <christian@serverless.industries>
2025-05-20 00:51:08 +0200

    add stream list and support for notes

--- src/_plugins/streamlist.rb
+++ src/_plugins/streamlist.rb
@@ -1,29 +1,33 @@
-module StreamPagePlugin
+module StreamListPagePlugin
 
-    class StreamPageGenerator < Jekyll::Generator
+    class StreamListPageGenerator < Jekyll::Generator
         safe true
 
         def generate(site)
-            site.data['stream'].each do |item|
+            streamdata = site.data['stream']
 
+            streamdata.each do |item|
                 cparts = item['created_at'].split("-")
                 item['itemyear'] = cparts[0]
                 item['itemmonth'] = cparts[1]
                 item['itemyearmonth'] = "#{cparts[0]}-#{cparts[1]}"
+            end
 
-                # create page object
-                site.pages << StreamPage.new(site, item)
+            mymonth = streamdata.group_by { |item| item['itemyearmonth'] }
 
+            mymonth.each do |key, items|
+                site.pages << StreamListPage.new(site, key, items)
             end
         end
     end
 
-    class StreamPage < Jekyll::Page
-        def initialize(site, item)
+    class StreamListPage < Jekyll::Page
+        def initialize(site, month, items)
             # properties
             @site = site
-            @iteminfo = item
             @base = site.source
+            @month = month
+            @items = items.sort_by { |item| item['created_at'] }.reverse
 
             # All pages have the same filename, so define attributes straight away.
             @basename = 'index'      # filename without the extension.
@@ -32,27 +36,24 @@ module StreamPagePlugin
 
             # Initialize data hash the page frontmatter
             @data = {
-                'title' => 'Stream item '+@iteminfo['id'],
-                'streamitem' => @iteminfo,
-                'streamitemid' => @iteminfo['id'],
-                'locale' => 'en',
-                'robot_noindex' => true
+                'title' => 'Stream items for '+@month,
+                'streamitems' => @items,
+                'locale' => 'en'
             }
 
             # Look up front matter defaults scoped to type `stream`, if given key
             # doesn't exist in the `data` hash.
             data.default_proc = proc do |_, key|
-                site.frontmatter_defaults.find(relative_path, :streams, key)
+                site.frontmatter_defaults.find(relative_path, :streamlists, key)
             end
         end
 
         # Placeholders that are used in constructing page URL
         def url_placeholders
             {
-                :streamitemid => @iteminfo['id'],
                 :basename   => basename,
-                :year   => @iteminfo['itemyear'],
-                :month   => @iteminfo['itemmonth'],
+                :year   => @items[0]['itemyear'],
+                :month   => @items[0]['itemmonth'],
                 :output_ext => output_ext
             }
         end
